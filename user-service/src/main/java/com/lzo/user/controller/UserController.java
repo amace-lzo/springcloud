10cc94c67f53b8495caf9f1ef8141f8bd559de77
zhaoyixiang <hljtlzyx@live.com>
2022-12-05 01:42:49 +0800

    Complete user service module

--- user-service/src/main/java/com/lzo/user/controller/UserController.java
+++ user-service/src/main/java/com/lzo/user/controller/UserController.java
@@ -2,19 +2,28 @@ package com.lzo.user.controller;
 
 import com.lzo.user.client.AuthClient;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController("user")
 public class UserController {
 
+    @Value("${foo}")
+    String foo;
+
     @Autowired
     private AuthClient authClient;
 
     @GetMapping("testFeign")
-    public void  testFeign(){
-        String result = authClient.getAuthKey();
-        System.out.println(result);
+    public String testFeign(){
+        return authClient.getAuthKey();
+    }
+
+    @RequestMapping(value = "/testConfig")
+    public String testConfig(){
+        return foo;
     }
 
 }
